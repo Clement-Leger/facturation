24b0020dbdccf914fb8590dc46a886da535d3f8a
Clement-Leger <clementleger4@gmail.com>
2023-04-13 16:10:37 +0200

    premieres bases de css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fact'App</title>
-</head>
-<body>
-    <h1>Exercice de facturation</h1>
+@extends('layout.layout')
 
-    <p>Premier test</p>
-</body>
-</html>
+
+
+
+@include('layout.end-layout')
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,7 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/css/app.css')
+    <title>Fact'App</title>
 </head>
 <body>
-    
+
+    <nav>
+        <label class="logo">Logo</label>
+
+            <ul>
+                <li><a class="active" href="#">Articles</a>
+                    <ul class="article-menu">
+                        <li><a href="#">Liste des articles</a></li>
+                        <li><a href="#">Liste de stock</a></li>
+                        <li><a href="#">Liste des prix</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Devis</a>
+                    <ul class="article-menu">
+                        <li><a href="#">Liste des articles</a></li>
+                        <li><a href="#">Liste de stock</a></li>
+                        <li><a href="#">Liste des prix</a></li>
+                    </ul></li>
+                <li><a href="#">Bons</a>
+                    <ul class="article-menu">
+                        <li><a href="#">Liste des articles</a></li>
+                        <li><a href="#">Liste de stock</a></li>
+                        <li><a href="#">Liste des prix</a></li>
+                    </ul></li>
+                <li><a href="#">Factures</a>
+                    <ul class="article-menu">
+                        <li><a href="#">Liste des articles</a></li>
+                        <li><a href="#">Liste de stock</a></li>
+                        <li><a href="#">Liste des prix</a></li>
+                    </ul></li>
+
+    </nav>
